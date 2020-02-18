e826b51013746a14f37a62d1e494fba377279d91
Ekler Peter <>
2020-02-18 23:13:52 +0100

    TicTacToe core functions

--- TicTacToe/app/src/main/java/hu/ait/tictactoe/ui/TicTacToeView.kt
+++ TicTacToe/app/src/main/java/hu/ait/tictactoe/ui/TicTacToeView.kt
@@ -4,18 +4,18 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.PointF
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
+import hu.ait.tictactoe.MainActivity
+import hu.ait.tictactoe.model.TicTacToeModel
 
 class TicTacToeView(contex: Context?, attrs: AttributeSet?) : View(contex, attrs) {
 
     var paintBackGround = Paint()
     var paintLine = Paint()
-
-    var x = -1
-    var y = -1
-
 
     init {
         paintBackGround.color = Color.BLACK
@@ -32,22 +32,81 @@ class TicTacToeView(contex: Context?, attrs: AttributeSet?) : View(contex, attrs
         canvas?.drawRect(0f, 0f, width.toFloat(),
             height.toFloat(), paintBackGround)
 
-        canvas?.drawLine(0f, 0f, width.toFloat(), height.toFloat(), paintLine)
+        drawBoard(canvas)
 
-        if (x != -1 && y != -1) {
-            canvas?.drawCircle(x.toFloat(), y.toFloat(), 60f, paintLine)
+        drawPlayers(canvas)
+    }
+
+    private fun drawBoard(canvas: Canvas?) {
+        // border
+        canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintLine)
+        // two horizontal lines
+        canvas?.drawLine(
+            0f, (height / 3).toFloat(), width.toFloat(), (height / 3).toFloat(),
+            paintLine
+        )
+        canvas?.drawLine(
+            0f, (2 * height / 3).toFloat(), width.toFloat(),
+            (2 * height / 3).toFloat(), paintLine
+        )
+
+        // two vertical lines
+        canvas?.drawLine(
+            (width / 3).toFloat(), 0f, (width / 3).toFloat(), height.toFloat(),
+            paintLine
+        )
+        canvas?.drawLine(
+            (2 * width / 3).toFloat(), 0f, (2 * width / 3).toFloat(), height.toFloat(),
+            paintLine
+        )
+    }
+
+    private fun drawPlayers(canvas: Canvas?) {
+        for (i in 0..2) {
+            for (j in 0..2) {
+                if (TicTacToeModel.getFieldContent(i, j) == TicTacToeModel.CIRCLE) {
+                    val centerX = (i * width / 3 + width / 6).toFloat()
+                    val centerY = (j * height / 3 + height / 6).toFloat()
+                    val radius = height / 6 - 2
+
+                    canvas?.drawCircle(centerX, centerY, radius.toFloat(), paintLine)
+                } else if (TicTacToeModel.getFieldContent(i, j) == TicTacToeModel.CROSS) {
+                    canvas?.drawLine((i * width / 3).toFloat(), (j * height / 3).toFloat(),
+                        ((i + 1) * width / 3).toFloat(),
+                        ((j + 1) * height / 3).toFloat(), paintLine)
+
+                    canvas?.drawLine(((i + 1) * width / 3).toFloat(), (j * height / 3).toFloat(),
+                        (i * width / 3).toFloat(), ((j + 1) * height / 3).toFloat(), paintLine)
+                }
+            }
         }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (event?.action == MotionEvent.ACTION_DOWN) {
-            x = event.x.toInt()
-            y = event.y.toInt()
+            // 0,1; 0,2...
+            val tX = event.x.toInt() / (width/3)
+            val tY = event.y.toInt() / (height/3)
 
-            invalidate() // redraws the view, the onDraw(...) will be called
+            if (tX < 3 && tY < 3 &&
+                TicTacToeModel.getFieldContent(tX, tY) == TicTacToeModel.EMPTY) {
+
+                TicTacToeModel.setFieldContent(tX, tY, TicTacToeModel.getNextPlayer())
+
+                TicTacToeModel.changeNextPlayer()
+
+                invalidate() // redraws the view, the onDraw(...) will be called
+
+                Toast.makeText((context as MainActivity), "win", Toast.LENGTH_LONG).show()
+            }
         }
 
         return true
+    }
+
+    public fun restart() {
+        TicTacToeModel.resetModel()
+        invalidate()
     }
 
 
