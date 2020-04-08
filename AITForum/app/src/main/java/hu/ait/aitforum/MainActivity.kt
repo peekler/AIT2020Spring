39385d6ea612982b37e523ebcdb4e239e739904c
Ekler Peter <>
2020-04-08 18:50:46 +0200

    HTTP demo: old API and Retrofit version

--- AITForum/app/src/main/java/hu/ait/aitforum/MainActivity.kt
+++ AITForum/app/src/main/java/hu/ait/aitforum/MainActivity.kt
@@ -1,7 +1,13 @@
 package hu.ait.aitforum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
